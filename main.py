8bc291e75f2b24701da1a549c60c306eff34d324
Valeriy Varlachev <valvarl@ya.ru>
2021-10-08 02:26:38 +0300

    class conversion

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
 # ! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import requests
-import random
-import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-
 from project.access import group_token, group_id
-
-
-def main():
-    replies = ['1', '2', '3']
-    bot_session = vk_api.VkApi(token=group_token)
-    bot_api = bot_session.get_api()
-    while True:
-        longpoll = VkBotLongPoll(bot_session, group_id)
-        try:
-            print('wait')
-            for event in longpoll.listen():
-                print('got event')
-                if event.type == VkBotEventType.MESSAGE_NEW:
-                    print('message')
-                    if event.from_user:
-                        print(event.message['from_id'])
-                        bot_api.messages.send(
-                            random_id=random.getrandbits(32),
-                            peer_id=event.message['peer_id'],
-                            message=random.choice(replies)
-                        )
-        except requests.exceptions.ReadTimeout as timeout:
-            continue
-
+from project.csgo_bot import CsGoBot
 
 if __name__ == '__main__':
-    main()
+    bot = CsGoBot(group_token, group_id)
+    bot.start()
